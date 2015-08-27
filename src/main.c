116401f7b24bfc2c6139bc55f92b22f2735581b1
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-08-27 03:20:52 +0200

    Added library for bfs

--- src/main.c
+++ src/main.c
@@ -1,13 +1,32 @@
 #include<stdio.h>
 #include "graph.h"
+#include "bfs.h"
+
+void run_testcase();
 
 graph *gPtr;
 
 int main()
 {
-    graph g;
-    gPtr = &g;
-    printf("Hello World");
-    initialize_graph(gPtr, false);
-		return 0;
+  int i;
+  int num_testcases;
+  scanf("%d", &num_testcases);
+  for (i = 0; i < num_testcases; i++) {
+    run_testcase();
+  }
+  return 0;
+}
+
+void run_testcase()
+{
+  int start_point;
+  graph g;
+  graph_ptr = &g;
+
+  initialize_graph(graph_ptr, false);
+  initialize_search(graph_ptr);
+
+  scanf("%d", &start_point);
+
+  bfs(graph_ptr, start_point);
 }
